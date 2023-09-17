cf056ab13b4135d1f5621198b07a401298b2a05c
Weria <weriaghazizadeh@gmail.com>
2023-09-17 18:30:26 -0400

    Add files via upload
    initial

---
+++ js/main.js
@@ -0,0 +1,86 @@
+jQuery(document).ready(function( $ ) {
+
+  // Smooth scroll for the menu and links with .scrollto classes
+  $('.smothscroll').on('click', function(e) {
+    e.preventDefault();
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      if (target.length) {
+
+        $('html, body').animate({
+          scrollTop: target.offset().top - 62
+        }, 1500, 'easeInOutExpo');
+      }
+    }
+  });
+
+  $('.carousel').carousel({
+    interval: 3500
+  });
+
+  // JavaScript Chart
+  var doughnutData = [{
+      value: 70,
+      color: "#1abc9c"
+    },
+    {
+      value: 30,
+      color: "#ecf0f1"
+    }
+  ];
+
+  // Bootstrap Chart
+  var doughnutData = [{
+    value: 90,
+    color: "#1abc9c"
+  },
+  {
+    value: 10,
+    color: "#ecf0f1"
+  }
+  ];
+
+  // WordPress Chart
+  var doughnutData = [{
+    value: 65,
+    color: "#1abc9c"
+  },
+  {
+    value: 35,
+    color: "#ecf0f1"
+  }
+  ];
+
+  // HTML Chart
+  var doughnutData = [{
+    value: 80,
+    color: "#1abc9c"
+  },
+  {
+    value: 20,
+    color: "#ecf0f1"
+  }
+  ];
+
+  // Photoshop Chart
+  var doughnutData = [{
+    value: 70,
+    color: "#1abc9c"
+  },
+  {
+    value: 30,
+    color: "#ecf0f1"
+  }
+  ];
+
+  // Illustrator Chart
+  var doughnutData = [{
+    value: 50,
+    color: "#1abc9c"
+  },
+  {
+    value: 50,
+    color: "#ecf0f1"
+  }
+  ];
+});
